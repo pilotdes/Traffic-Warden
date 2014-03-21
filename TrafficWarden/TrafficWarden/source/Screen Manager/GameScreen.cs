fe3b2e5f4690bdaf1106caea426a8b5bab688a07
pilotdes <bomberdes@live.com>
2014-03-21 15:17:22 +1300

    Started on the Screenmanagers

---
+++ TrafficWarden/TrafficWarden/source/Screen Manager/GameScreen.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TrafficWarden.source.Screen_Manager
+{
+    public abstract class GameScreen
+    {
+    }
+}

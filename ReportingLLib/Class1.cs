7d88bdf323a496f50ea0fdb5e2c546103b2c1bad
pilotdes <bomberdes@live.com>
2014-03-21 14:29:35 +1300

    removed libary...

--- ReportingLLib/Class1.cs
+++ ReportingLLib/Class1.cs
@@ -1,12 +1,39 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics.Eventing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace ReportingLLib
 {
-    public class Class1
+    public class Report
     {
+        protected static UnhandledExceptionEventHandler handler;
+
+        public static void InitReporting()
+        {
+            handler =
+new UnhandledExceptionEventHandler(Target)
+        }
+
+        private static void Target(object sender, UnhandledExceptionEventArgs unhandledExceptionEventArgs)
+        {
+            printToLog((sender as Exception), unhandledExceptionEventArgs);
+            outWriter.WriteLine();
+            PrintToOutput("**********EXCEPTION**********");
+            PrintToOutput("See ErrorLog For Details");
+            outWriter.WriteLine();
+            Exit();
+        }
+
+        private static void WriteErrorLog()
+        {
+            
+        }
+        public static void EndReporting()
+        {
+            
+        }
     }
 }
